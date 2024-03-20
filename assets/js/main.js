e19941b7bd14675d6d15b07f4b6a4907c0db6a43
chisomije <chisomije92@gmail.com>
2024-03-20 19:05:34 +0100

    set up clients section

--- assets/js/main.js
+++ assets/js/main.js
@@ -167,6 +167,36 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   /**
+   * Clients Slider
+   */
+  new Swiper(".clients-slider", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 40,
+      },
+      480: {
+        slidesPerView: 3,
+        spaceBetween: 60,
+      },
+      640: {
+        slidesPerView: 4,
+        spaceBetween: 80,
+      },
+      992: {
+        slidesPerView: 6,
+        spaceBetween: 120,
+      },
+    },
+  });
+  /**
    * Animation on scroll function and init
    */
   function aos_init() {
