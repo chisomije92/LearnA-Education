bc5f6bd504acb5002aaebc6abde98d9d23f29ebc
chisomije <chisomije92@gmail.com>
2024-03-20 19:13:13 +0100

    set up services and testimonials

--- assets/js/main.js
+++ assets/js/main.js
@@ -196,6 +196,43 @@ document.addEventListener("DOMContentLoaded", () => {
       },
     },
   });
+
+  /**
+   * Testimonials Slider
+   */
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  /**
+   * Testimonials Slider
+   */
+  new Swiper(".portfolio-details-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
   /**
    * Animation on scroll function and init
    */
